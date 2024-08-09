fda2564d7306c535e6a403e654b576e3a0846678
Deon Blaauw <deon@visight.tech>
2024-08-09 22:31:47 +0200

    Added new spawner, easier, can create using code instead of UI which is much easier when supporting multiple worlds

---
+++ Enemies/EnemySpawnData.gd
@@ -0,0 +1,70 @@
+# EnemySpawnData.gd
+extends Resource
+
+class_name EnemySpawnData
+
+@export var spawn_data: Array[Dictionary] = [
+	{
+		"enemy_scene": preload("res://Enemies/enemy_bat.tscn"),
+		"time_start": 0,
+		"time_end": 30,
+		"spawn_delay": 0,
+		"spawn_amount": 2
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_kobold_weak.tscn"),
+		"time_start": 30,
+		"time_end": 210,
+		"spawn_delay": 0,
+		"spawn_amount": 4
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_kobold_strong.tscn"),
+		"time_start": 60,
+		"time_end": 180,
+		"spawn_delay": 1,
+		"spawn_amount": 4
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_cyclops.tscn"),
+		"time_start": 180,
+		"time_end": 210,
+		"spawn_delay": 2,
+		"spawn_amount": 4
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_kobold_strong.tscn"),
+		"time_start": 210,
+		"time_end": 240,
+		"spawn_delay": 0,
+		"spawn_amount": 4
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_cyclops.tscn"),
+		"time_start": 240,
+		"time_end": 270,
+		"spawn_delay": 0,
+		"spawn_amount": 2
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_juggernaut.tscn"),
+		"time_start": 240,
+		"time_end": 240,
+		"spawn_delay": 0,
+		"spawn_amount": 2
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_cyclops.tscn"),
+		"time_start": 270,
+		"time_end": 300,
+		"spawn_delay": 0,
+		"spawn_amount": 10
+	},
+	{
+		"enemy_scene": preload("res://Enemies/enemy_super.tscn"),
+		"time_start": 300,
+		"time_end": 300,
+		"spawn_delay": 0,
+		"spawn_amount": 1
+	}
+]
--- Utility/enemy_spawner.gd
+++ Utility/enemy_spawner.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-@export var spawns: Array[Spawn_Info] = []
-@onready var player = get_tree().get_first_node_in_group("player")
+@export var spawn_data_resource: Resource
 
-@export var time = 0
+@onready var player = get_tree().get_first_node_in_group("player")
+var time = 0
 
 signal changetime(time)
 
 func _ready():
-	connect("changetime",Callable(player,"change_time"))
+	connect("changetime", Callable(player, "change_time"))
 
 func _on_timer_timeout():
 	time += 1
@@ -16,29 +16,35 @@ func _on_timer_timeout():
 	changetime.emit(time)
 	
 func spawn_enemies():
-	var enemy_spawns = spawns
-	for i in enemy_spawns:
-		if time >= i.time_start and time <= i.time_end:
-			if i.spawn_delay_counter < i.spawn_delay:
-				i.spawn_delay_counter += 1
+	if not spawn_data_resource:
+		return
+
+	var spawn_data = spawn_data_resource.spawn_data
+	
+	for spawn_info in spawn_data:
+		if time >= spawn_info["time_start"] and time <= spawn_info["time_end"]:
+			if "spawn_delay_counter" not in spawn_info:
+				spawn_info["spawn_delay_counter"] = 0
+			
+			if spawn_info["spawn_delay_counter"] < spawn_info["spawn_delay"]:
+				spawn_info["spawn_delay_counter"] += 1
 			else:
-				i.spawn_delay_counter = 0
-				var new_enemy = i.enemy
+				spawn_info["spawn_delay_counter"] = 0
+				var new_enemy_scene = spawn_info["enemy_scene"]
 				var counter = 0
-				while counter < i.spawn_amount:
-					var enemy_spawn = new_enemy.instantiate()
+				while counter < spawn_info["spawn_amount"]:
+					var enemy_spawn = new_enemy_scene.instantiate()
 					enemy_spawn.global_position = get_random_position()
 					add_child(enemy_spawn)
 					counter += 1
 				
-				
 func get_random_position():
-	var vpr = get_viewport_rect().size * randf_range(1.1,1.4)
-	var top_left = Vector2(player.global_position.x - vpr.x/2,player.global_position.y - vpr.y/2)
-	var top_right = Vector2(player.global_position.x + vpr.x/2,player.global_position.y - vpr.y/2)
-	var bottom_left = Vector2(player.global_position.x - vpr.x/2,player.global_position.y + vpr.y/2)
-	var bottom_right = Vector2(player.global_position.x + vpr.x/2,player.global_position.y + vpr.y/2)
-	var pos_side = ["up","down","left","right"].pick_random()
+	var vpr = get_viewport_rect().size * randf_range(1.1, 1.4)
+	var top_left = Vector2(player.global_position.x - vpr.x / 2, player.global_position.y - vpr.y / 2)
+	var top_right = Vector2(player.global_position.x + vpr.x / 2, player.global_position.y - vpr.y / 2)
+	var bottom_left = Vector2(player.global_position.x - vpr.x / 2, player.global_position.y + vpr.y / 2)
+	var bottom_right = Vector2(player.global_position.x + vpr.x / 2, player.global_position.y + vpr.y / 2)
+	var pos_side = ["up", "down", "left", "right"].pick_random()
 	var spawn_pos1 = Vector2.ZERO
 	var spawn_pos2 = Vector2.ZERO
 	
@@ -54,9 +60,9 @@ func get_random_position():
 			spawn_pos2 = bottom_right
 		"left":
 			spawn_pos1 = top_left
-			spawn_pos2 = bottom_left	
-			
-	var x_spawn = randf_range(spawn_pos1.x,spawn_pos2.x)
-	var y_spawn = randf_range(spawn_pos1.y,spawn_pos2.y)	
+			spawn_pos2 = bottom_left
 	
-	return Vector2(x_spawn,y_spawn)
+	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
+	var y_spawn = randf_range(spawn_pos1.y, spawn_pos2.y)
+	
+	return Vector2(x_spawn, y_spawn)
