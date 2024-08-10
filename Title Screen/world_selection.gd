28702b1f323d27e2c4068dcbc8abee364c2a64c0
Deon Blaauw <deon@visight.tech>
2024-08-10 22:39:32 +0200

    Added 10 min scene

--- Title Screen/world_selection.gd
+++ Title Screen/world_selection.gd
@@ -1,32 +1,42 @@
 extends Control
-@onready var grasslands_tile = $Grasslands
-@onready var acacia_tile = $Acacia
+
+@onready var grasslands_tile = $GrasslandsTile
+@onready var acacia_tile = $AcaciaTile
+@onready var inferius_tile = $InferiusTile
 
 var lvl_menu_char_selection = "res://Title Screen/character_selection.tscn"
 
 var grasslands_scene = "res://World/grasslands_world.tscn"
 var acacia_scene = "res://World/acacia_world.tscn"
+var inferius_scene = "res://World/infernius_world.tscn"
 
 func _ready():
 	# Grasslands setup
 	var world_name = "Grasslands"
-	var texture_path = "res://Assets/Textures/dirt_full_new.png"
+	var icon_texture_path = "res://Assets/Textures/dirt_full_new.png"
 	var description = "Grassy lands, bats and monsters. Grab your pearls!"
 	var game_type = "Duration: 5 minutes"
-	setup_world_tile(grasslands_tile, world_name, description, game_type, texture_path)
+	setup_world_tile(grasslands_tile, world_name, description, game_type, icon_texture_path)
 	
 	# Acacia setup
 	world_name = "Acacia"
-	texture_path = "res://Assets/Textures/World/backgrounds 256x256/256_Dirt Muddy 01.png"
+	icon_texture_path = "res://Assets/Textures/World/backgrounds 256x256/256_Dirt Muddy 01.png"
 	description = "Endless monsters. Stone flooring. What's not to love!?"
 	game_type = "Duration: 5 minutes (harder)"
-	setup_world_tile(acacia_tile, world_name, description, game_type, texture_path)
+	setup_world_tile(acacia_tile, world_name, description, game_type, icon_texture_path)
+
+	# Infernius setup
+	world_name = "Infernius"
+	icon_texture_path = "res://Assets/Textures/World/backgrounds 256x256/256_Grass 03 Mud.png"
+	description = "Follow your heart, young adventurer. Unless you're Bob, then you're just old."
+	game_type = "Duration: 10 minutes (exp)"
+	setup_world_tile(inferius_tile, world_name, description, game_type, icon_texture_path)
 
 	
-func setup_world_tile(world_node, world_name, description, game_type, texture_path):
+func setup_world_tile(world_node, world_name, description, game_type, icon_texture_path):
 	world_node.lbl_title.text = world_name.capitalize()
 	world_node.lbl_description.text = description
-	world_node.icon.texture = load(texture_path)
+	world_node.icon.texture = load(icon_texture_path)
 	world_node.lbl_game_type.text = game_type
 	
 func _on_grasslands_world_select_click_end():
@@ -38,3 +48,7 @@ func _on_btn_back_click_end():
 
 func _on_acacia_world_select_click_end():
 	get_tree().change_scene_to_file(acacia_scene)
+
+
+func _on_inferius_tile_world_select_click_end():
+	get_tree().change_scene_to_file(inferius_scene)
